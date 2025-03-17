b6aff33a3e8ca555031f91f0d446019186326bea
FAmanca <fadlyamanca@gmail.com>
2025-03-17 08:01:50 +0700

    bikin login register

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Exception;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        try {
+            $validate = Validator::make($request->all(),[
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+
+            if($validate->fails()) {
+                return response()->json([
+                    'message' => 'Invalid Data',
+                    'errors' => $validate->errors()
+                ], 422);
+            }
+
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'message' => 'Unauthorized'
+                ], 401);
+            }
+
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            $user->access_token = $token;
+            $user->token_type = 'Bearer';
+
+            return response()->json([
+                'message' => 'Login Berhasil',
+                'data' => $user
+            ]);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Internal Server Error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function register(Request $request) {
+        try {
+            $validate = Validator::make($request->all(),[
+                'name' => 'required',
+                'email' => 'required',
+                'no_telp' => 'required',
+                'password' => 'required',
+            ]);
+
+            if($validate->fails()) {
+                return response()->json([
+                    'message' => 'Invalid Data',
+                    'errors' => $validate->errors()
+                ], 422);
+            }
+
+            $user = new User();
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $user->no_telp = $request->input('no_telp');
+            $user->password = $request->input('password');
+            $user->save();
+
+            return response()->json([
+                'message' => 'Registrasi Berhasil',
+                'data' => $user
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Internal Server Error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
