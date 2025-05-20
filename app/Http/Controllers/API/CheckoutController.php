8198ece8334ac305ac970ca2fbde1d693c3a09bc
RichardNixon990 <richardnixon990@gmail.com>
2025-05-20 11:17:16 +0700

    finish checkout

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -6,6 +6,7 @@ use Exception;
 use App\Models\Transaction;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\DetailTransaction;
 use App\Http\Controllers\Controller;
 
 class CheckoutController extends Controller
@@ -21,12 +22,13 @@ class CheckoutController extends Controller
                 ], 422);
             }
 
+
             $totalBerat = $user->cart->cart_detail->sum(function ($detail) {
-                return $detail->product->berat;
+                return $detail->product->berat * $detail->jumlah;
             });
 
             $transaction = new Transaction();
-            $transaction->user_id = auth()->id();
+            $transaction->user_id = $user->id;
             $transaction->status = "Proses";
             $transaction->tanggal_transaksi = now();
             $transaction->kode_transaksi = 'SJK-' . time() . strtoupper(Str::random(5));
@@ -34,8 +36,31 @@ class CheckoutController extends Controller
             $transaction->total_berat = $totalBerat;
             $transaction->save();
 
-            // $transaction->id; ambil id
+            foreach ($user->cart->cart_detail as $cartDetail) {
+                DetailTransaction::create([
+                    'transaction_id' => $transaction->id,
+                    'product_id' => $cartDetail->product_id,
+                    'harga' => $cartDetail->harga / $cartDetail->jumlah,
+                    'jumlah' => $cartDetail->jumlah,
+                    'subtotal' => $cartDetail->harga,
+                    'totalberat' => $cartDetail->product->berat * $cartDetail->jumlah,
+                ]);
 
+
+                $cartDetail->product->stock -= $cartDetail->jumlah;
+                $cartDetail->product->save();
+            }
+
+
+            $user->cart->cart_detail()->delete();
+            $user->cart->total_harga = 0;
+            $user->cart->total_jumlah = 0;
+            $user->cart->save();
+
+            return response()->json([
+                'message' => 'Checkout berhasil',
+                'data' => $transaction->load('detail_transaction.product')
+            ]);
         } catch (Exception $e) {
             return response()->json([
                 'message' => 'Internal Server Error',
