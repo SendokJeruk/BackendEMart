1be78912a8dd49de0ef336a00b99d834786c0e57
famanca <fadlyamanca@gmail.com>
2026-03-28 09:17:35 +0700

    verify mail page yg ketinggalan + ganti mail method

--- app/Mail/ResetPasswordEmail.php
+++ app/Mail/ResetPasswordEmail.php
@@ -41,7 +41,7 @@ class ResetPasswordEmail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.verify-email',
+            view: 'emails.reset-pass',
         );
     }
 
